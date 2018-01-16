7fc670ecebf307f4e2ba2a763625a9153d1d2d2e
Jeremie Aubut <jeremie.aubut@gmail.com>
2018-01-15 22:48:37 -0500

    adding video on chanv.co

---
+++ src/pages/apropos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="Apropos">
+    <Bloc class="span-2">
+      <Texte>
+        <h2>Chanvre Québec</h2>
+        <p>Nous on parle de chanvre et on aime ça.</p>
+      </Texte>
+    </Bloc>
+  </div>
+</template>
+<script>
+import Bloc from '@/components/bloc'
+import Texte from '@/components/index/text'
+
+export default {
+  name: 'Apropos',
+  components: {
+    Bloc,
+    Texte
+  }
+}
+</script>
+<style lang="sass" scoped>
+  #Apropos
+    display: grid
+    grid-template-columns: 1fr 1fr
+    grid-auto-rows: calc(100vh/1.7)
+    grid-gap: 10px
+    width: 100%
+</style>
