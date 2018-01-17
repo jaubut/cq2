7e5dcc625b82482b30d569b509ee3122cc31f4e8
Jeremie Aubut <jeremie.aubut@gmail.com>
2018-01-17 18:41:26 -0500

    allo ajoue de la section equipe

---
+++ src/components/Personequipe.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="person">
+    <img :src="person.fields.image.fields.file.url" :alt="person.fields.name">
+    <h3>{{ person.fields.name }}</h3>
+    <p>{{ person.fields.shortBio }}</p>
+    <p>you can follow {{ person.fields.name }} on <span><a target="_blank" :href="'https://twitter.com/'+person.fields.twitter"><i class="fab fa-twitter"></i></a></span></p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Person',
+  props: ['person']
+}
+</script>
+<style lang="sass" scoped>
+  #person
+    display: flex
+    flex-flow: column wrap
+    justify-content: center
+    align-items: center
+    img
+      height: 150px
+      width: auto
+      border-radius: 100%
+
+</style>
--- src/pages/apropos.vue
+++ src/pages/apropos.vue
@@ -6,17 +6,70 @@
         <p>Nous on parle de chanvre et on aime ça.</p>
       </Texte>
     </Bloc>
+    <Bloc class="beige">
+      <Texte>
+        <h2>Notre Mission</h2>
+        <p>Notre mission est de faire de la marde.</p>
+      </Texte>
+    </Bloc>
+    <Bloc>
+      <Texte>
+        <h2>Notre Vision</h2>
+        <p>Notre vision est que nous sommes la tête et non la queue du père à amé.</p>
+      </Texte>
+    </Bloc>
+    <Bloc class="span-2">
+      <Texte>
+        <h2>Notre Équipe</h2>
+      </Texte>
+    </Bloc>
+    <template v-for="person in persons">
+      <Bloc :key="person.title" class="bloc">
+        <Texte>
+          <h2>{{ person.fields.name }}</h2>
+          <p>{{ person.fields.shortBio }}</p>
+          <p>you can follow {{ person.fields.name }} on <span><a target="_blank" :href="'https://twitter.com/'+person.fields.twitter"><i class="fab fa-twitter"></i></a></span></p>
+        </Texte>
+      </Bloc>
+      <Bloc :key="person.title" :style="{'background-image': 'url(' + person.fields.image.fields.file.url + ')'}"></Bloc>
+    </template>
   </div>
 </template>
 <script>
+import {createClient} from '../../utils/contentful-api'
 import Bloc from '@/components/cqbloc'
 import Texte from '@/components/texte'
+
+const client = createClient()
 
 export default {
   name: 'Apropos',
   components: {
     Bloc,
     Texte
+  },
+  data () {
+    return {
+      persons: []
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  watch: {
+    'lang': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      return client.getEntries({
+        'content_type': 'author',
+        order: '-sys.createdAt'
+      })
+        .then(response => {
+          this.persons = response.items
+          return response
+        })
+    }
   }
 }
 </script>
@@ -28,5 +81,5 @@ export default {
     grid-gap: 10px
     width: 100%
     @media(max-width:468px)
-      grid-auto-rows: minmax(calc(100vh/1.7), auto) 
+      grid-auto-rows: minmax(calc(100vh/1.7), auto)
 </style>
