0dfe7794e41e0bd8437ad083e81b9b1c0ac1906e
jaubut <jeremie.aubut@gmail.com>
2018-01-23 12:39:30 -0500

    correcting webpack config for imgix support with webpack

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -95,6 +95,9 @@ const webpackConfig = merge(baseWebpackConfig, {
       {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
+        transform: function (content, path) {
+          return content.replace('/static', 'https://cq2.imgix.net')
+        },
         ignore: ['.*']
       }
     ]),
