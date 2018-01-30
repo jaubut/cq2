a396eb8d235e2c2fc64c19398c36a60ce5df5712
jaubut <jeremie.aubut@gmail.com>
2018-01-30 15:18:08 -0500

    changement du load de facebook
    voila les cons

--- src/main.js
+++ src/main.js
@@ -72,17 +72,6 @@ router.beforeEach((to, from, next) => {
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  created: function () {
-    (function (d, s, id) {
-      var js
-      var fjs = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) return
-      js = d.createElement(s)
-      js.id = id
-      js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9'
-      fjs.parentNode.insertBefore(js, fjs)
-    }(document, 'script', 'facebook-jssdk'))
-  },
   router,
   store,
   template: '<App/>',
